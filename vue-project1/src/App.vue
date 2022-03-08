88aeb76786211763b82298c5c6eb933560ef33fe
Lee Joo Hyung <wngud985@gmail.com>
2022-03-08 10:38:50 +0900

    modal 적용 및 사진 추가

--- vue-project1/src/App.vue
+++ vue-project1/src/App.vue
@@ -1,38 +1,43 @@
 <template>
   <div>
+    <div class="black-bg" v-if="modal === true">
+      <div class="white-bg">
+        <h4>상세 페이지 입니다</h4>
+        <p>
+          상세 페이지 내용입니다
+          <button @click="modal = false">닫기</button>
+        </p>
+      </div>
+    </div>
+
     <div class="menu">
       <a v-for="(작명, index) in menus" v-bind:key="index">{{ 작명 }}</a>
     </div>
 
-    <div>
-      <h4>{{ products[0] }}</h4>
-      <p>xx 만원</p>
-      <button @click="신고수[0] += 1">허위 매물 신고</button>
-      <span> 신고수: {{ 신고수[0] }}</span>
-    </div>
-    <div>
-      <h4>{{ products[1] }}</h4>
-      <p>xx 만원</p>
-      <button @click="신고수[1]++">허위 매물 신고</button>
-      <span> 신고수: {{ 신고수[1] }}</span>
-    </div>
-    <div>
-      <h4>{{ products[2] }}</h4>
-      <p>xx 만원</p>
-      <button @click="신고수[2]++">허위 매물 신고</button>
-      <span> 신고수: {{ 신고수[2] }}</span>
+    <div v-for="(el, index) in products" v-bind:key="index">
+      <img @click="modal = true" class="room-img" :src="el.image" />
+      <h4>{{ el.title }}</h4>
+      <p>{{ el.price }}</p>
+      <p>{{ el.content }}</p>
+      <button @click="report1++">허위 매물 신고</button>
+      <span> 신고수: {{ report1 }}</span>
     </div>
   </div>
 </template>
 
 <script>
+import room from "./assets/oneroom.js";
+
 export default {
   name: "App",
   data() {
     return {
-      신고수: [0, 0, 0],
+      modal: false,
+      report1: 0,
+      report2: 0,
+      report3: 0,
       menus: ["home", "shop", "about"],
-      products: ["역삼동우너룸", "천호동원룸", "마포구원룸"],
+      products: room,
     };
   },
   methods: {},
@@ -42,6 +47,34 @@ export default {
 </script>
 
 <style>
+.room-img {
+  width: 50%;
+  margin-top: 40px;
+}
+
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 10px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
